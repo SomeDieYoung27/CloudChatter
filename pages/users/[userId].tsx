a22bfd97946fa189ce98bf8ab9f0c87ae09b16b9
SomeDieYoung27 <111681395+SomeDieYoung27@users.noreply.github.com>
2023-09-08 13:05:56 +0530

    Update [userId].tsx

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,12 +1,14 @@
-import Header from "@/components/Header";
-import useUser from "@/hooks/useUser";
 import { useRouter } from "next/router";
 import { ClipLoader } from "react-spinners";
-import UserHero from "@/components/users/userHero";
-import UserBio from "@/components/users/UserBio";
-import PostFeed from "@/components/posts/PostFeed";
 
-const userView = () => {
+import useUser from "@/hooks/useUser";
+
+import PostFeed from "@/components/posts/PostFeed";
+import Header from "@/components/Header";
+import UserBio from "@/components/users/UserBio";
+import UserHero from "@/components/users/userHero";
+
+const UserView = () => {
   const router = useRouter();
   const { userId } = router.query;
 
@@ -14,14 +16,7 @@ const userView = () => {
 
   if (isLoading || !fetchedUser) {
     return (
-      <div
-        className="
-                     flex
-                     justify-center
-                     items-center
-                     h-full
-            "
-      >
+      <div className="flex justify-center items-center h-full">
         <ClipLoader color="lightblue" size={80} />
       </div>
     );
@@ -29,11 +24,12 @@ const userView = () => {
 
   return (
     <>
-      <Header shootBackArrow label={fetchedUser?.name} />
+      <Header showBackArrow label={fetchedUser?.name} />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
       <PostFeed userId={userId as string} />
     </>
   );
 };
-export default userView;
+
+export default UserView;
