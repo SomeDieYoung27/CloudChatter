7098c9d41508697da5487b48763963dcb189acee
SomeDieYoung27 <111681395+SomeDieYoung27@users.noreply.github.com>
2023-09-08 13:07:11 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,11 @@ import { useCallback } from "react";
 import { BiArrowBack } from "react-icons/bi";
 
 interface HeaderProps {
+  showBackArrow?: boolean;
   label: string;
-  shootBackArrow?: boolean;
 }
 
-const Header: React.FC<HeaderProps> = ({ label, shootBackArrow }) => {
+const Header: React.FC<HeaderProps> = ({ showBackArrow, label }) => {
   const router = useRouter();
 
   const handleBack = useCallback(() => {
@@ -15,18 +15,18 @@ const Header: React.FC<HeaderProps> = ({ label, shootBackArrow }) => {
   }, [router]);
 
   return (
-    <div className="border-b-[1px] border-nuetral-800 p-5">
+    <div className="border-b-[1px] border-neutral-800 p-5">
       <div className="flex flex-row items-center gap-2">
-        {shootBackArrow && (
+        {showBackArrow && (
           <BiArrowBack
             onClick={handleBack}
             color="white"
             size={20}
             className="
-                    cursor-pointer
-                    hover:opacity-70
-                    transition
-                  "
+              cursor-pointer 
+              hover:opacity-70 
+              transition
+          "
           />
         )}
         <h1 className="text-white text-xl font-semibold">{label}</h1>
@@ -34,4 +34,5 @@ const Header: React.FC<HeaderProps> = ({ label, shootBackArrow }) => {
     </div>
   );
 };
+
 export default Header;
